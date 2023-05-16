bd579e45df220f2dc32678cce22ae726f428d8ed
takeshimaru <ezotishearts@hotmail.co.jp>
2023-05-16 21:53:54 +0900

    Create home.py

---
+++ app/home.py
@@ -0,0 +1,10 @@
+import streamlitas st
+import pandasas pd
+import numpyas np
+
+def app():
+    st.title('Home')
+    st.write("This is a sample home page in the mutliapp.")
+    st.write("See `apps/home.py` to know how to use it.")
+    st.markdown("### Sample Data")
+    st.write('Navigate to `Data Stats` page to visualize the data')
